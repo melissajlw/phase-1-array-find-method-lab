738d9a85a4c8a638a0ee5e7e0ff54b068b80869b
Melissa Wang <melissa.wang21@gmail.com>
2024-09-03 11:07:56 -0400

    array find method

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+// function superbowlWin(record) 
+//  - returns a year the Denver Broncos won the superbowl
+//  - returns undefined if the record has no win objects
+
+function superbowlWin(record) {
+    const found = record.find(o => o.result == "W")
+    if (found) {
+        return found.year
+    } else {
+        return found
+    }
+}
+
